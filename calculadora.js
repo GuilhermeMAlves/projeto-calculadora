d5b45301e86e45ed0b4fa3f3541098a3baaea69b
GUILHERME MARTINS ALVES <guilherme.malves11@senacsp.edu.br>
2020-02-28 11:16:15 -0300

    calculadora simples

---
+++ calculadora.js
@@ -0,0 +1,62 @@
+var result = document.getElementById("result");
+var n1=0, n2=0
+var operacao=""
+
+function onNumberClicked(numberButton){
+    
+    if(result.innerHTML == "0"){
+        result.innerHTML = numberButton.innerHTML;
+    }
+    else{
+        result.innerHTML += numberButton.innerHTML;
+    }
+}
+
+function onClearClicked(){
+    result.innerHTML = "0"
+    n1=0
+    n2=0
+}
+
+function onEqualsClicked(){
+    n2=parseInt(result.innerHTML)
+    if (operacao=="+") {
+        result.innerHTML=n1+n2
+    }
+    if (operacao=="-") {
+        result.innerHTML=n1-n2
+    }
+    if (operacao=="*") {
+        result.innerHTML=n1*n2
+    }
+    if (operacao=="/") {
+        if(n2>0) {
+            result.innerHTML=n1/n2
+        }
+        else (result.innerHTML="Impossivel a divisão por zero.")
+    }    
+}
+
+function onDivideClicked(){
+    n1=parseInt(result.innerHTML)
+    operacao="/"
+    result.innerHTML = "0"
+}
+
+function onAddClicked(){
+    n1=parseInt(result.innerHTML)
+    operacao="+"
+    result.innerHTML = "0"
+}
+
+function onSubtractClicked(){
+    n1=parseInt(result.innerHTML)
+    operacao="-"
+    result.innerHTML = "0"
+}
+
+function onMultiplyCliked(){
+    n1=parseInt(result.innerHTML)
+    operacao="*"
+    result.innerHTML = "0"
+}
